15e2879fe1c8fc602c7516ef4becabf9e8119908
raffa <raffalevy@me.com>
2016-12-14 18:41:58 -0500

    Fixed javadoc comments

--- src/com/raphaellevy/cs1/rockpaperscissors/ConsoleMenu.java
+++ src/com/raphaellevy/cs1/rockpaperscissors/ConsoleMenu.java
@@ -6,8 +6,8 @@ package com.raphaellevy.cs1.rockpaperscissors;
 import java.util.Scanner;
 
 /**
- * @author raffa
  * A menu that is displayed in the console, allowing for choice between multiple options.
+ * @author raffa
  */
 abstract class ConsoleMenu extends RPSComponent{
 	ConsoleMenu(Main mainClass) {
--- src/com/raphaellevy/cs1/rockpaperscissors/RPSComponent.java
+++ src/com/raphaellevy/cs1/rockpaperscissors/RPSComponent.java
@@ -3,8 +3,8 @@ package com.raphaellevy.cs1.rockpaperscissors;
 import java.util.Scanner;
 
 /**
- * @author raffa
  * Most classes extend this other than {@link Main}. Contains important information such as the {@link Scanner} and Main instance.
+ * @author raffa
  */
 abstract class RPSComponent {
 	protected Scanner scan;

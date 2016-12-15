7fffa1a42d5c5832de66e40a1402ab1a2aa9c2b0
raffa <raffalevy@me.com>
2016-12-14 20:56:40 -0500

    Added computer name generator in HelperMethods.java

--- src/com/raphaellevy/cs1/rockpaperscissors/HelperMethods.java
+++ src/com/raphaellevy/cs1/rockpaperscissors/HelperMethods.java
@@ -1,10 +1,20 @@
 package com.raphaellevy.cs1.rockpaperscissors;
 
+import java.util.Random;
+
 /**
  * Contains static methods to be used by other classes.
  * 
  * @author raffa
  */
 class HelperMethods {
-
+	static Random rand = new Random();
+	
+	/**
+	 * @return A cool name for a computer opponent
+	 */
+	static String computerName() {
+		String[] names = {"Appy","Javabot","RockPaperScissorsMaster","Grandmaster"};
+		return Integer.toHexString(rand.nextInt(0xFFFF)) + names[rand.nextInt(names.length)] + Integer.toHexString(rand.nextInt(0xFFFF));
+	}
 }
